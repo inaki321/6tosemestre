88c6a1a5957e9f6cc95906d9a40043384adb3721
Inaki321 <inaki00@hal9.ai>
2022-04-03 15:19:35 -0500

    v1.3

--- arquitectura.py
+++ arquitectura.py
@@ -1,6 +1,109 @@
 # -*- coding: utf-8 -*-
 import math
 import time
+from datetime import date, datetime
+from os.path import exists
+
+   
+def Instrucciones(inputIn):
+
+   if(inputIn == 'suma'):
+       resA = suma()
+       print('suma: ',resA)
+       with open(dtime, "a") as f:
+           f.write(str(resA))
+           f.write('\n')
+           f.write(str(inputNumerico))
+           f.write('\n')
+
+   if(inputIn == 'resta'):
+       resA = resta()
+       print('resta: ',resA)
+       with open(dtime, "a") as f:
+           f.write(str(resA))
+           f.write('\n')
+           f.write(str(inputNumerico))
+           f.write('\n')
+
+   if(inputIn == 'multiplicacion'):
+       resA = multiplica()
+       print('multiplicacion: ',resA)
+       with open(dtime, "a") as f:
+           f.write(str(resA))
+           f.write('\n')
+           f.write(str(inputNumerico))
+           f.write('\n')
+
+   if(inputIn == 'division'):
+       resA = divide()
+       print('division: ',resA)
+       with open(dtime, "a") as f:
+           f.write(str(resA))
+           f.write('\n')
+           f.write(str(inputNumerico))
+           f.write('\n')
+
+   if(inputIn == 'factorial'):
+       resA = factorial()
+       print('factorial: ',resA)
+       with open(dtime, "a") as f:
+           f.write(str(resA))
+           f.write('\n')
+           f.write(str(inputNumerico))
+           f.write('\n')
+           
+   if(inputIn == 'seno'):
+       resA = seno()
+       print('factorial: ',resA)
+       with open(dtime, "a") as f:
+           f.write(str(resA))
+           f.write('\n')
+           f.write(str(inputNumerico))
+           f.write('\n')
+
+   if(inputIn == 'coseno'):
+       resA = coseno()
+       print('factorial: ',resA)
+       with open(dtime, "a") as f:
+           f.write(str(resA))
+           f.write('\n')
+           f.write(str(inputNumerico))
+           f.write('\n')
+           
+   if(inputIn == 'tangente'):
+       resA = tan()
+       print('factorial: ',resA)
+       with open(dtime, "a") as f:
+           f.write(str(resA))
+           f.write('\n')
+           f.write(str(inputNumerico))
+           f.write('\n')
+
+   if(inputIn == 'duplica'):
+      duplica()
+   if(inputIn == 'duplica n'):
+       duplicaN()
+   if(inputIn == 'intercambia'):
+       intercambia()
+   if(inputIn == 'intercambia n'):
+       intercambiaN()
+   if(inputIn == 'duerme'):
+       duerme()
+   if(inputIn == 'duerme n'):
+       duermeN()
+   if(inputIn == 'muestra '):
+       muestra()
+   if(inputIn == 'promedio n'):
+       promedioN()
+   if(inputIn == 'comentario '):
+       comentario()
+   if(inputIn == 'ecuacion 2 grado'):
+       ec2grado()
+   if(inputIn == 'memoria'):
+       memoria()
+   if(inputIn == 'leer'):
+       leerTxt()
+
 
 def suma():
     if(len(inputNumerico) > 1 ):
@@ -78,86 +181,104 @@ def borradofunc():
     elif borrado == True:
         borrado = False
 
-def seno(x):
+def seno():
     signo=1
     salida=0
     for i in range(1,11,2):
-        salida = salida +(signo*pow(x,i)/math.factorial(i))
+        salida = salida +(signo*pow(inputNumerico[-1],i)/math.factorial(i))
         signo = -signo
     
     return salida
 
-def coseno(x):
+def coseno():
     signo=1
     salida=0
     for i in range(0,12,2):
-        salida = salida +(signo*pow(x,i)/math.factorial(i))
+        salida = salida +(signo*pow(inputNumerico[-1],i)/math.factorial(i))
         signo = -signo
     
     return salida
 
-def tan(x):
+def tan():
     salida = 0
-    salida = seno(x)/coseno(x)
+    salida = seno(inputNumerico[-1])/coseno(inputNumerico[-1])
     return salida
 
-def duplica(array):
-    if (len(array) >= 1):
-        dup = array[-1]
+def raiz():
+    if(inputNumerico>=1):
+        n = math.sqrt(inputNumerico[-1])
+        if(borrado==True):
+            inputNumerico.pop()
+        inputNumerico.append(n)
+    else:
+        print('Error, faltan datos')
+
+def logaritmo():
+    if(inputNumerico>=1):
+        n = math.log(inputNumerico[-1]) #logaritmo natural
+        if(borrado==True):
+            inputNumerico.pop()
+        inputNumerico.append(n)
+    else:
+        print('Error, faltan datos')    
+
+def duplica():
+    if (len(inputNumerico) >= 1):
+        dup = inputNumerico[-1]
     
         print('Duplicando: ' + dup)
-        array.append(dup)
+        inputNumerico.append(dup)
     else:
-        print('Faltan datos') 
+        print('Faltan datos')
         
-def duplicaN(array):
-    if (array.len() >= 2):
-        n = array[-1]
-        valor = array[-2]
+def duplicaN():
+    if (inputNumerico.len() >= 2):
+        n = inputNumerico[-1]
+        valor = inputNumerico[-2]
     
         print('Duplicando: ' + valor)
         print('Veces: ' + n)
         
         for i in range(0,n):
-            array.append(valor)
+            inputNumerico.append(valor)
         
     else:
         print('Faltan datos')
 
-def intercambia(array):
-    if (len(array) >= 2):
-        n1 = array[-1]
-        n2 = array[-2]
+def intercambia():
+    if (len(inputNumerico) >= 2):
+        n1 = inputNumerico[-1]
+        n2 = inputNumerico[-2]
     
-        array.pop()
-        array.pop()
+        inputNumerico.pop()
+        inputNumerico.pop()
         
-        array.append(n1)
-        array.append(n2)
+        inputNumerico.append(n1)
+        inputNumerico.append(n2)
     else:
         print('Faltan datos')
 
-def intercambiaN(array):
-    if (len(array) >= 3):
-        pos = array[-1]-1
-        n = array[-2]
-        array.pop()
+def intercambiaN():
+    if (len(inputNumerico) >= 3):
+        pos = inputNumerico[-1]-1
+        n = inputNumerico[-2]
+        inputNumerico.pop()
         
         if (pos >= 0):
-            b = array[pos]
-            array.insert(pos,n)
+            b = inputNumerico[pos]
+            inputNumerico.insert(pos,n)
         else:
             for i in range(0,pos):
-                array.append(0)
-            array.insert(len(array)-1+pos,n)
-        array.pop()
-        array.append(b)
+                inputNumerico.append(0)
+            inputNumerico.insert(len(inputNumerico)-1+pos,n)
+        inputNumerico.pop()
+        inputNumerico.append(b)
     else:
         print('Error al intercambiar, faltan datos')
 
-def duermeN(array):
-    if(len(array >= 1)):
-        n = array[-1]
+def duermeN():
+    if(len(inputNumerico >= 1)):
+        n = inputNumerico[-1]
         
         print('Durmiendo ' + n + 'segundos')
         time.sleep(n)
@@ -171,25 +292,25 @@ def duerme():
     except:
         print('Ocurrio un error al dormir')
 
-def muestra(array):
+def muestra():
     if (borrado==True):
-        print('*Con Borrado*')
+        print('****Con Borrado****')
     else:
-        print('*Sin Borrado*')
-    for i, value in enumerate(array):
-        print(value + '.- ' +array)
+        print('****Sin Borrado****')
+    for i, value in enumerate(inputNumerico):
+        print(value + '.- ' +inputNumerico)
 
-def promedioN(array):
-    if(len(array) >= 1):
-        n = array[-1]
-        if(len(array)>=n+1):
+def promedioN():
+    if(len(inputNumerico) >= 1):
+        n = inputNumerico[-1]
+        if(len(inputNumerico)>=n+1):
             suma = 0
             for i in range(0,n):
-                suma += array[len(array)-2-i]
+                suma += inputNumerico[len(inputNumerico)-2-i]
         if (borrado == True):
             for j in range(0,n):
-                array.pop()
-        array.append(suma/n)
+                inputNumerico.pop()
+        inputNumerico.append(suma/n)
     else:
         print('Error al promediar, faltan datos')
 
@@ -201,11 +322,11 @@ def comentario():
         print('Ocurrio un error')
 
 
-def ec2grado(array):
-    if (len(array)>=3):
-        a=array[-3]
-        b=array[-2]
-        c=array[-1]
+def ec2grado():
+    if (len(inputNumerico)>=3):
+        a=inputNumerico[-3]
+        b=inputNumerico[-2]
+        c=inputNumerico[-1]
         
         d = b**2 - (4*a*c)
         if (d<0):
@@ -215,23 +336,43 @@ def ec2grado(array):
             x2=(-b-math.sqrt(d))/(2*a)
             if (borrado==True):
                 for i in range(0,3):
-                    array.pop()
-            array.append(x1)
-            array.append(x2)
+                    inputNumerico.pop()
+            inputNumerico.append(x1)
+            inputNumerico.append(x2)
     else:
         print('Error al resolver Ec 2 grado, faltan datos')
 
-def memoria(array):
-    if(len(array)>0):
-        array.append(len(array))
+def memoria():
+    if(len(inputNumerico)>0):
+        inputNumerico.append(len(inputNumerico))
     else:
         print('Error, no hay suficientes datos')
 
+def leerTxt():
+    with open('ejemplo.txt') as f:
+        lines = f.readlines()
+    
+    for i in range(0,len(lines)):
+        arrayAux = lines[i].split()
+        instruccionAux = arrayAux[-1]
+        del arrayAux[-1]
+        for j in arrayAux:
+            inputNumerico.append(float(j))
+        Instrucciones(instruccionAux)
 
+
+#-----------------------MAIN---------------------------------#
+now = datetime.now()
+dtime = now.strftime("%d%m%Y%H%M")
+dtime =  dtime+'.txt'
+file_exists = exists(dtime)
+
+if not file_exists:
+    f= open(dtime,"w+")   
+     
 inputInstruccion = 'x'
 inputNumerico = []
 borrado = False
-
 
 while inputInstruccion != 'salir' :
     
@@ -239,9 +380,9 @@ while inputInstruccion != 'salir' :
    
    salir = 'No'
    
-   noNumInstrucciones = ['salir', '*','duplica','intercambia'] #instrucciones que no necesitan input de numero
-   
-   if(inputInstruccion in noNumInstrucciones):
+   noNumInstrucciones = ['salir', '*','duplica','intercambia','leer'] #instrucciones que no necesitan input de numero
+
+   if(inputInstruccion not in noNumInstrucciones):
        while(salir != 'si'):
            try:
                aux = float(input('Dame los numeros: '))
@@ -256,48 +397,15 @@ while inputInstruccion != 'salir' :
            print('No hay borrado')
        elif borrado == True:
            print('Ya hay borrado')
-
-   if(inputInstruccion == 'suma'):
-       print('la suma fue: ',suma())
-   if(inputInstruccion == 'resta'):
-       print('la resta fue: ',resta())
-   if(inputInstruccion == 'multiplicacion'):
-       print('la multiplicacion fue: ',multiplica())
-   if(inputInstruccion == 'division'):
-       print('la division fue: ',divide())
-   if(inputInstruccion == 'factorial'):
-       print('el factorial fue: ',factorial())
-   if(inputInstruccion == 'seno'):
-       print('el seno fue: ',seno())
-   if(inputInstruccion == 'coseno'):
-       print('el coseno fue: ',coseno())
-   if(inputInstruccion == 'tangente'):
-       print('el tangente fue: ',tan())
-   if(inputInstruccion == 'duplica'):
-      duplica()
-   if(inputInstruccion == 'duplica n'):
-       duplicaN()
-   if(inputInstruccion == 'intercambia'):
-       intercambia()
-   if(inputInstruccion == 'intercambia n'):
-       intercambiaN()
-   if(inputInstruccion == 'duerme'):
-       duerme()
-   if(inputInstruccion == 'duerme n'):
-       duermeN()
-   if(inputInstruccion == 'muestra '):
-       muestra()
-   if(inputInstruccion == 'promedio n'):
-       promedioN()
-   if(inputInstruccion == 'comentario '):
-       comentario()
-   if(inputInstruccion == 'ecuacion 2 grado'):
-       ec2grado()
-   if(inputInstruccion == 'memoria'):
-       memoria()
-
-    
+           
    
+   with open(dtime, "a") as f:
+       f.write('\n')
+       f.write(str(inputInstruccion))
+       f.write('\n')      
+       f.write(str(inputNumerico))
+       f.write('\n')
    
-   
-   
+   Instrucciones(inputInstruccion)
+   if(inputInstruccion=='salir'):
+       f.close()
